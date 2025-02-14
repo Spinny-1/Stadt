55e471f8d449a5288cb25d0f1410bd588b192673
Spinny-1 <gianluca.gontow@gmail.com>
2025-02-14 18:04:20 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
-let image1;
+let image1, image2;
+let currentImage;  // Variable für das aktuell angezeigte Bild
 
 function preload() {
-  image1 = loadImage("StadtPost.png");  // Bild laden
+  image1 = loadImage("StadtPast.png");  // Bild 1 laden
+  image2 = loadImage("StadtPost.png");  // Bild 2 laden
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);  // Leinwand erstellen
-  image1.resize(width, height);  // Bild auf die Größe der Leinwand skalieren
+  image1.resize(width, height);  // Bild 1 skalieren
+  image2.resize(width, height);  // Bild 2 skalieren
+  currentImage = image1;  // Standardmäßig Bild 1 anzeigen
 }
 
 function draw() {
   background(255);  // Hintergrund auf weiß setzen
-  image(image1, 0, 0);  // Bild anzeigen
+  image(currentImage, 0, 0);  // Das aktuelle Bild anzeigen
+}
+
+function keyPressed() {
+  if (key === '1') {
+    currentImage = image1;  // Bild 1 anzeigen
+  } else if (key === '2') {
+    currentImage = image2;  // Bild 2 anzeigen
+  }
 }
